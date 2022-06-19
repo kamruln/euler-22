e72f4f00bacaf8bec2cbe8fd6017474f57b9199e
Ashar Naser <asharnaser@energysmartschool.org>
2022-06-19 10:42:34 -0400

    euler 22

---
+++ euler-22-problem.js
@@ -0,0 +1,20 @@
+/*Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file? */
+
+
+
+const fs = require('fs');
+
+const nameScore = function(filename)  {
+    const fileData = fs.readFileSync(filename, 'utf8');
+    const nameArray = fileData.replace(/\"/g, '').split(',');
+    console.log(fileData)
+    console.log(nameArray)
+    return 5
+}
+
+const score = nameScore("names.txt")
+console.log("Score is", score)
